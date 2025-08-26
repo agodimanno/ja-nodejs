943673078d791f35d4f3e64b5a614a0305990b25
Bolinius <andry-96thebest@hotmail.it>
2025-08-26 16:43:46 +0200

    Test2

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,10 +4,6 @@ test('greets the user by name', () => {
   expect(greet('DevOps')).toBe('Ciao, DevOps!');
 });
 
-test('cacazione', () => {
-  expect(nicolle()).toBe('Cacati addosso!');
-});
-
 // Extra messages and error simulation tests
 describe("Extra messages and error simulation", () => {
   test("should display extra messages", () => {
--- app.js
+++ app.js
@@ -2,8 +2,4 @@ function greet(name) {
   return `Ciao, ${name}!`;
 }
 
-function nicolle(){
-  return "Cacati addosso!";
-}
-
 module.exports = { greet, nicolle };
