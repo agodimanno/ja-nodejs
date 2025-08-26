4c47e5cd80e6e40c2036de76e20086e8497ea9a2
Bolinius <andry-96thebest@hotmail.it>
2025-08-26 16:39:18 +0200

    Aggiunta test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,3 +3,16 @@ const greet = require('../app');
 test('greets the user by name', () => {
   expect(greet('DevOps')).toBe('Ciao, DevOps!');
 });
+
+// Extra messages and error simulation tests
+describe("Extra messages and error simulation", () => {
+  test("should display extra messages", () => {
+    console.log("Messaggio extra 1");
+    console.log("Messaggio extra 2");
+    expect(true).toBeTruthy();
+  });
+
+  test("simulate failure", () => {
+    throw new Error("Errore simulato per il test");
+  });
+});
